b0d7e0559e7220e3185066efbf16434101871a4d
blythdsouza <blythdsouza@gmail.com>
2026-08-22 14:31:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,3 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { useState } from 'react';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthProvider, useAuth } from './context/AuthContext';
